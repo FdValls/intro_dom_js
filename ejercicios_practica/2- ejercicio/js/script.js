027a5df4afaff07c1ba486baba454147b42b3667
Fdvalls <fernandodanielvalls@gmail.com>
2023-05-28 19:24:23 -0300

    continuando ejercicios

--- ejercicios_practica/2- ejercicio/js/script.js
+++ ejercicios_practica/2- ejercicio/js/script.js
@@ -17,3 +17,16 @@ Obtener todos los botones utilizando la clase "btn",
 luego a todos los botones agregar la clase "agua"
 
 */
+
+//Ejercicio 1 
+const btn1 = document.getElementById("boton1").innerText = "PEGAR";
+const btn2 = document.getElementById("boton2").innerText = "COPIAR";
+const btn3 = document.getElementById("boton3").innerText = "CORTAR";
+
+//Ejercicio 2
+const btns = document.querySelectorAll(".btn")
+console.log(btns)
+
+
+
+
