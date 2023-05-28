6e4e49d7f83de8412e1ec6ae5637b98f986ad49b
Fdvalls <fernandodanielvalls@gmail.com>
2023-05-28 19:06:39 -0300

    comenzando ejercicios

--- ejercicios_practica/1- ejercicio/js/script.js
+++ ejercicios_practica/1- ejercicio/js/script.js
@@ -7,10 +7,17 @@
 
 /* Enunciado
 
-1)
+1)(
 Leer el título del "header" del post e imprimirlo en consola.   
 
 2)
 Cambiar título utilizando javascript por otro cualquiera.
 
 */
+
+//Ejercicio 1
+const titleHeader = document.querySelector('.post-header')
+console.log(titleHeader)
+
+//Ejercicio 2
+titleHeader.textContent = "HelloWorld"
